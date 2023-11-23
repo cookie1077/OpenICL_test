10fa640bf3c59c5fb3b17d9dfa3e7d7efacaa252
cookie1077 <cookie1077@naver.com>
2023-11-24 01:23:06 +0900

    icl test

--- openicl/icl_inferencer/icl_ppl_inferencer.py
+++ openicl/icl_inferencer/icl_ppl_inferencer.py
@@ -82,6 +82,9 @@ class PPLInferencer(BaseInferencer):
             ice.append(retriever.generate_ice(ice_idx_list[idx], ice_template=ice_template))
         output_handler.save_ice(ice)
 
+        print(ice)
+        print('-----------------')
+
         # Here important : need to see !
         # 5. Calculating PPL for prompts in each label's class
         for label in labels:
@@ -122,7 +125,7 @@ class PPLInferencer(BaseInferencer):
                     normalizing_prompt_list.append(normalizing_prompt)
                 prompt_list.append(prompt)
 
-            print(prompt_list[0])
+            #print(prompt_list[0])
 
             if normalizing_str is not None:
                 normalizing_str_len = self.get_input_token_num(normalizing_str)
