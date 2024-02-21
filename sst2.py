d49fb2b431d864b762ad2a784c1047d20c4bdf53
cookie1077 <cookie1077@naver.com>
2024-02-21 18:44:24 +0900

    add binning

--- sst2.py
+++ sst2.py
@@ -106,6 +106,51 @@ def test_sequence(ice_num, data):
     
     return score
 
+
+def test_binning(ice_num, data):
+
+    # ICL exemplar template
+    ice_dict = "</E>Movie Review: </text> </Label1> is very likely, </Label2> is not very likely"
+
+    # Inference prompt template
+    tp_dict = {
+        '0' : "</E>Movie Review: </text> Negative",
+        '1' : "</E>Movie Review: </text> Positive"
+    }
+
+    label_dict = {
+        '0': "Negative",
+        '1': "Positive"
+    }
+
+
+    column_token_map = {
+        'text': '</text>', 
+        'Label1' : '</Label1>', 
+        'Label2' : '</Label2>' 
+    }
+
+    ice_template = PromptTemplate(ice_dict, column_token_map, label_dict=label_dict, ice_token='</E>')
+    prompt_template = PromptTemplate(tp_dict, {'text': '</text>'}, ice_token='</E>')
+
+
+    from openicl import RandomRetriever
+    # Define a retriever using the previous `DataLoader`.
+    # `ice_num` stands for the number of data in in-context examples.
+    retriever = RandomRetriever(data, ice_num=ice_num, labels= ['0', '1'], order=True)
+
+    from openicl import PPLInferencer
+    inferencer = PPLInferencer(model_name='distilgpt2', labels= ['0', '1'])
+
+    from openicl import AccEvaluator
+    # the inferencer requires retriever to collect in-context examples, as well as a template to wrap up these examples.
+    predictions = inferencer.inference(retriever, ice_template=ice_template, prompt_template=prompt_template)
+    # compute accuracy for the prediction
+    score = AccEvaluator().score(predictions=predictions, references=data.references)
+    
+    return score
+
+
 def test_GT(ice_num, data):
 
     # Inference prompt template
@@ -175,23 +220,26 @@ def test_pseudo_GT(ice_num, data):
     
     return score
 
-shots = 20
-naive, sequence, gt, pseudo_gt = [], [], [], []
+shots = 10
+naive, sequence, binning, gt, pseudo_gt = [], [], [], [], []
 x = [n for n in range(shots)]
 
 for i in range(shots):
     naive.append(test_naive(i, data)['accuracy'])
     sequence.append(test_sequence(i, data)['accuracy'])
+    binning.append(test_binning(i, data)['accuracy'])
     gt.append(test_GT(i, data)['accuracy'])
     pseudo_gt.append(test_pseudo_GT(i, data)['accuracy'])
 
 print(naive)
 print(sequence)
+print(binning)
 print(gt)
 print(pseudo_gt)
 
 plt.plot(x, naive, label = 'naive')
 plt.plot(x, sequence, label = 'sequence')
+plt.plot(x, binning, label = 'binning')
 plt.plot(x, gt, label = 'gt')
 plt.plot(x, pseudo_gt, label = 'pseudo_gt')
 
--- sst5.py
+++ sst5.py
@@ -1,6 +1,7 @@
 from datasets import load_dataset
 from datasets import Dataset, DatasetDict
 from openicl import DatasetReader
+import matplotlib.pyplot as plt
 import json
 import vessl 
 
@@ -11,9 +12,9 @@ def gen(file_path):
         for line in f:
             yield json.loads(line)
             
-train_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/sst2/train_spaced_sst2.jsonl"})
-val_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/sst2/dev.jsonl"})
-test_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/sst2/test.jsonl"})
+train_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/sst5/train_spaced_sst5.jsonl"})
+val_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/sst5/dev.jsonl"})
+test_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/sst5/test.jsonl"})
 
 dataset_dict = DatasetDict({"train": train_ds, "validation": val_ds, "test": test_ds})
 dataset = dataset_dict 
@@ -116,13 +117,56 @@ def test_sequence(ice_num, data):
     
     return score
 
-def test_GT(ice_num, data):
+
+def test_binning(ice_num, data):
 
     # ICL exemplar template
-    ice_dict = "</E>Movie Review: </text> Positive </P>% Negative </N>%"
+    ice_dict = "</E>Movie Review: </text> </Label1> </1>% </Label2> </2>% </Label3> </3>% </Label4> </4>% </Label5> </5>%"
 
     # Inference prompt template
-    ice_dict2 = {
+    tp_dict = {
+        '0' : "</E>Movie Review: </text> Very Negative",
+        '1' : "</E>Movie Review: </text> Negative",
+        '2' : "</E>Movie Review: </text> Neutral" ,
+        '3' : "</E>Movie Review: </text> Positive" ,
+        '4' : "</E>Movie Review: </text> Very Positive" 
+    }
+
+    label_dict = {
+        '0': "Very Negative",
+        '1': "Negative",
+        '2': "Neutral",
+        '3': "Positive",
+        '4': "Very Positive"
+    }
+
+
+    column_token_map = {'text': '</text>', 0 : '</1>', 'Label1' : '</Label1>', 1 : '</2>', 'Label2' : '</Label2>',2 : '</3>', 'Label3' : '</Label3>',3 : '</4>', 'Label4' : '</Label4>',4 : '</5>', 'Label5' : '</Label5>' }
+    ice_template = PromptTemplate(ice_dict, column_token_map, label_dict=label_dict, ice_token='</E>')
+    prompt_template = PromptTemplate(tp_dict, {'text': '</text>'}, ice_token='</E>')
+
+
+    from openicl import RandomRetriever
+    # Define a retriever using the previous `DataLoader`.
+    # `ice_num` stands for the number of data in in-context examples.
+    retriever = RandomRetriever(data, ice_num=ice_num, labels= ['0', '1', '2', '3', '4'], order=True)
+
+    from openicl import PPLInferencer
+    inferencer = PPLInferencer(model_name='distilgpt2', labels= ['0', '1', '2', '3', '4'])
+
+    from openicl import AccEvaluator
+    # the inferencer requires retriever to collect in-context examples, as well as a template to wrap up these examples.
+    predictions = inferencer.inference(retriever, ice_template=ice_template, prompt_template=prompt_template)
+    # compute accuracy for the prediction
+    score = AccEvaluator().score(predictions=predictions, references=data.references)
+    
+    return score
+
+
+def test_GT(ice_num, data):
+
+    # Inference prompt template
+    ice_dict = {
         0 : "</E>Movie Review: </text> Very Negative",
         1 : "</E>Movie Review: </text> Negative",
         2 : "</E>Movie Review: </text> Neutral" ,
@@ -138,8 +182,8 @@ def test_GT(ice_num, data):
         4 : "</E>Movie Review: </text> Very Positive" 
     }
 
-    column_token_map = {'text': '</text>', '0' : '</P>', '1' : '</N>' }
-    ice_template = PromptTemplate(ice_dict2, column_token_map, ice_token='</E>')
+    column_token_map = {'text': '</text>'}
+    ice_template = PromptTemplate(ice_dict, column_token_map, ice_token='</E>')
     prompt_template = PromptTemplate(tp_dict, {'text': '</text>'}, ice_token='</E>')
 
 
@@ -160,23 +204,65 @@ def test_GT(ice_num, data):
     return score
 
 
-naive = []
-sequence = []
-gt = []
-x = [n for n in range(5)]
+def test_pseudo_GT(ice_num, data):
 
-for i in range(5):
+    # Inference prompt template
+    ice_dict = {
+        0 : "</E>Movie Review: </text> Very Negative",
+        1 : "</E>Movie Review: </text> Negative",
+        2 : "</E>Movie Review: </text> Neutral" ,
+        3 : "</E>Movie Review: </text> Positive" ,
+        4 : "</E>Movie Review: </text> Very Positive" 
+    }
+
+    tp_dict = {
+        0 : "</E>Movie Review: </text> Very Negative",
+        1 : "</E>Movie Review: </text> Negative",
+        2 : "</E>Movie Review: </text> Neutral" ,
+        3 : "</E>Movie Review: </text> Positive" ,
+        4 : "</E>Movie Review: </text> Very Positive" 
+    }
+
+    column_token_map = {'text': '</text>'}
+    ice_template = PromptTemplate(ice_dict, column_token_map, ice_token='</E>')
+    prompt_template = PromptTemplate(tp_dict, {'text': '</text>'}, ice_token='</E>')
+
+
+    from openicl import RandomRetriever
+    # Define a retriever using the previous `DataLoader`.
+    # `ice_num` stands for the number of data in in-context examples.
+    retriever = RandomRetriever(data, ice_num=ice_num, labels= [0,1,2,3,4] )
+
+    from openicl import PPLInferencer
+    inferencer = PPLInferencer(model_name='distilgpt2', labels= [0,1,2,3,4])
+
+    from openicl import AccEvaluator
+    # the inferencer requires retriever to collect in-context examples, as well as a template to wrap up these examples.
+    predictions = inferencer.inference(retriever, ice_template=ice_template, prompt_template=prompt_template, pseudo_gt='pseudo_gt')
+    # compute accuracy for the prediction
+    score = AccEvaluator().score(predictions=predictions, references=data.references)
+    
+    return score
+
+shots = 10
+naive, sequence, gt, pseudo_gt = [], [], [], []
+x = [n for n in range(shots)]
+
+for i in range(shots):
     naive.append(test_naive(i, data)['accuracy'])
     sequence.append(test_sequence(i, data)['accuracy'])
     gt.append(test_GT(i, data)['accuracy'])
+    pseudo_gt.append(test_pseudo_GT(i, data)['accuracy'])
 
 print(naive)
 print(sequence)
 print(gt)
+print(pseudo_gt)
 
-import matplotlib.pyplot as plt
-
-plt.plot(x, sequence)
-plt.plot(x, gt)
+plt.plot(x, naive, label = 'naive')
+plt.plot(x, sequence, label = 'sequence')
+plt.plot(x, gt, label = 'gt')
+plt.plot(x, pseudo_gt, label = 'pseudo_gt')
 
 plt.legend()
+plt.savefig('/output/sst5.png')
